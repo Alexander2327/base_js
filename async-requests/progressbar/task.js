7730ba5cc94d9c02f1d0c74e6932713fe0e730b6
Alexander2327 <danilov.aa1@mail.ru>
2022-11-06 15:37:53 +0300

    progress

---
+++ async-requests/progressbar/task.js
@@ -0,0 +1,12 @@
+const form = document.forms.form;
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
+
+    xhr.onprogress = function(event) {        
+        progress.value = event.loaded / 10000;
+    }
+    xhr.send(formData);
+})
